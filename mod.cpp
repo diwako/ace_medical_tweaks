3472b48b96140e0969a39187a52f126f70737679
diwako <diwako@gmail.com>
2020-10-07 17:09:23 +0200

    release setup and license

---
+++ mod.cpp
@@ -0,0 +1,15 @@
+name = "Diwako's ACE medical tweaksm";
+dir = "@diwako_ace_medical_tweaks";
+author = "diwako";
+
+tooltipOwned = "Diwako's ACE medical tweaks";
+description = "Diwako's ACE medical tweaks";
+picture = "logo.paa";
+logoSmall = "logo.paa";
+logo = "logo.paa";
+logoOver = "logo.paa";
+hidePicture = 0;
+hideName = 0;
+actionName = "Website";
+action = "https://github.com/diwako";
+overview = "Several adjustable tweaks to ACE medical.";
